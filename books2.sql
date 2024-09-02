c8fe12e09b585120af5e59841fcb16c20f27b9bc
Juhan Heinlaid <98657402+D1ZEASE420@users.noreply.github.com>
2024-09-02 13:53:08 +0300

    Add files via upload

---
+++ books2.sql
@@ -0,0 +1,3 @@
+SELECT * FROM d124543_bookstore.books
+WHERE release_date >= 2010 AND type = "new"
+ORDER BY release_date;
---
+++ books3.sql
@@ -0,0 +1,2 @@
+SELECT * FROM d124543_bookstore.books
+WHERE release_date < 1970 AND type = "used" AND price < 20;
